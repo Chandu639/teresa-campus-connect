30be108131c0b6b7af56426b36f54b6a7eaeb6ef
Siva369IT <sivasatyamsetti369@gmail.com>
2025-09-07 13:32:54 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -152,7 +152,7 @@ const Index = () => {
             <div className="logo-container animate-fade-in">
               <div className="logo-image">
                 <img 
-                  src="../assets/logo.jpg" 
+                  src="@/assets/logo.jpg" 
                   alt="Mother Teresa High School Logo" 
                   className="w-full h-full object-contain"
                 />
